b7d7a24d67c0a4ef59aaf57f09c1ee68193292c2
Prime <jeff.d.937@gmail.com>
2021-10-21 13:03:41 -0400

    ' edit schema'

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,27 @@
 DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS   role;
-DROP TABLE IF EXISTS department;
+DROP TABLE IF         role;
+DROP TABLE IF EXISTS departments;
 
-CREATE TABLE department{
+CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    department_id VARCHAR(30) NOT NULL,
     description TEXT
 );
 
-}
+CREATE TABLE role (
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    tittle VARCHAR(30) NOT NULL,
+    department_id VARCHAR(30) INTEGER NOT NULL,
+    salary DECIMAL(8.0) NOT NULL
+   
+);
+
+CREATE TABLE employee (
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+   roll id  INTEGER,
+   first_name VARCHAR(30) NOT NULL,
+   last_name VARCHAR(30) NOT NULL,
+    manager_id INTEGER,
+);
+
