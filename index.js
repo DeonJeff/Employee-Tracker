4ad00025960574341439df005c66fa98d305b86c
Prime <jeff.d.937@gmail.com>
2021-10-24 20:36:35 -0400

    'load prompt +'

--- index.js
+++ index.js
@@ -12,5 +12,56 @@ function loadPrompts() {
       choices:  [ "View Employees", "View Employees by Department", "Add Employee", "Remove Employees",  "Update Employee Role", "Add Role", "End"]
 
     })
-        
-    }
+
+    .then(function (res) {
+        switch (res.start) {
+            case "View all Employees":
+                displayEmployees();
+                break;
+
+                case "View all Departments":
+                displayDepartments();
+                break;
+
+                case "View all Roles":
+                displayRoles();
+                break;
+
+                case "View all Employees by Deparment":
+                displayEmpbyDepartment();
+                break;
+
+                case "View all Employees by Manager":
+                displayEmbyManager();
+                break;
+
+                case "Add Employee":
+                addEmployee();
+                break;
+
+                case "Remove Employee":
+                removeEmployee();
+                break;
+
+                case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+
+                case "Remove Role":
+                 removeRole();
+                break;
+
+                case "Add New Department":
+                    addDepartment();
+                break;
+
+                case "Remove Department":
+                    removeDept();
+                    break;
+
+                case "Update Employee Manager":
+                    updateemployeeManager();
+                    break;
+ }
+    })
+         };
