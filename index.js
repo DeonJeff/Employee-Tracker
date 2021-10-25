6e2ab2504cfc49453b41eeeb9b40c33eee58151b
Prime <jeff.d.937@gmail.com>
2021-10-24 23:02:50 -0400

    'done'

--- index.js
+++ index.js
@@ -69,12 +69,14 @@ function loadPrompts() {
          // View employees
          function viewEmployee() {
             
-            const Query = "SELECT employee.id  , employee.first_name, employee.last_name, role.title, department.name AS department, role.salary" 
-              CONCAT(manager.first_name,"" , manager.last_name) AS manager, 
-              FROM employee,  
-              LEFT JOIN role ON employee.role_id = role.id, 
-               LEFT JOIN department ON role.department_id = department.id, 
-                LEFT JOIN employee manager ON  employee.manager_id = manager.id"
+        const Query = "SELECT employee.id  , employee.first_name, employee.last_name, role.title, department.name
+        AS department role.salary,
+    
+              CONCAT(manager.first_name,"" , manager.last_name) AS manager 
+              department.name FROM employee  
+              LEFT JOIN role ON employee.role_id = role.id 
+               LEFT JOIN department ON role.department_id = department.id 
+                LEFT JOIN employee manager ON  employee.manager_id = manager.id;"
           
             connection.query(Query, (err, data) => {
                 if (err) throw err;
